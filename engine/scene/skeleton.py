78b9aadf99618c543941140f62bb4cab01926ec0
Žygimantas Šlikas <zygimantas.slikas1999@gmail.com>
2023-06-01 17:37:54 +0300

    final lab 3

--- engine/scene/skeleton.py
+++ engine/scene/skeleton.py
@@ -7,31 +7,28 @@ import glm
 class Bone:
     def __init__(self, scene_object, parent, position, scale):
         self.parent:Bone = parent
+        self.children:list[Bone] = []
+
         self.start:np.ndarray = np.array(position, dtype=np.float32)
         self.length = glm.vec3(0, scale[1], 0)
         self.end:np.ndarray = None
-        self.rotation = np.array([0, 0, 0], dtype=np.float32)
-        self.transformation_matrix:np.ndarray
-        self.scene_object:SceneObject = scene_object
-        self.create_matrix()
 
-    def create_matrix(self) -> np.ndarray:
+        self.rotation = np.array([0, 0, 0], dtype=np.float32)
+        self.transformation_matrix:np.ndarray = self.create_matrix(self.rotation)
+        self.scene_object:SceneObject = scene_object
+        self.scene_object.position = self.start
+
+    def create_matrix(self, rotation:np.ndarray) -> np.ndarray:
         rotation_axis = glm.vec3(1,0,0)
-        rotation = glm.rotate(self.rotation[0], rotation_axis)
+        rotation_matrix = glm.rotate(rotation[0], rotation_axis)
 
         rotation_axis = glm.vec3(0,1,0)
-        rotation @= glm.rotate(self.rotation[1], rotation_axis)
+        rotation_matrix @= glm.rotate(rotation[1], rotation_axis)
 
         rotation_axis = glm.vec3(0,0,1)
-        rotation @= glm.rotate(self.rotation[2], rotation_axis)
+        rotation_matrix @= glm.rotate(rotation[2], rotation_axis)
 
-        # shift = glm.vec3(self.start)
-        transformation = glm.translate(rotation, self.length)
-        self.transformation_matrix = transformation
-        self.end = transformation @ self.start
-        parent = self.parent
-        # if parent is not None:
-        #     self.transformation_matrix = parent.transformation_matrix @ self.transformation_matrix
+        return rotation_matrix
 
     def get_position(self) -> np.ndarray:
         if self.parent is None:
@@ -48,51 +45,95 @@ class Bone:
             position = position @ glm.vec3(parent.start)
         return position
 
+    def set_rotation(self, rotation:np.ndarray)->None:
+        self.rotation = rotation
+        self.transformation_matrix = self.create_matrix(self.rotation)
+        self.update_tale()
+
+    def set_position(self, new_start:np.ndarray)->None:
+        self.start = new_start
+        self.update_tale()
+
+    def update_tale(self) -> None:
+        cumulative_rotation = self.rotation.copy()
+        parent = self.parent
+        while parent is not None:
+            cumulative_rotation += parent.rotation
+            parent = parent.parent
+        self.end = self.start + self.create_matrix(cumulative_rotation) @ self.length
+        self.scene_object.rotation = cumulative_rotation
+        self.scene_object.position = self.start
+        for child in self.children:
+            child.set_position(self.end)
+
 
 class Skeleton:
     def __init__(self, mesh, light, position:tuple[float, float, float]):
         self.position = position
         self.scene_objects = []
 
-        self.bones = []
+        self.bones:list[Bone] = []
         self.rotations = []
         self.create_bones(mesh, light)
 
     def create_bones(self, mesh, light) -> None:
         for i in range(5):
-            sc_1 = SceneObject(mesh, light, position=(0,0,0), scale=(0.3, 1, 0.3))
+            if i != 4:
+                sc_1 = SceneObject(mesh, light, position=(0,0,0), scale=(0.2, 1, 0.2))
+            else :
+                sc_1 = SceneObject(mesh, light, position=(0,0,0), scale=(0.1, 1, 0.1))
             self.scene_objects.append(sc_1)
             if len(self.bones) == 0:
                 bone_parent = None
+                bone_1 = Bone(sc_1, bone_parent, position=(0,i,0), scale=(0.3, 1, 0.3))
             else :
                 bone_parent = self.bones[-1]
-            bone_1 = Bone(sc_1, bone_parent, position=(0,i,0), scale=(0.3, 1, 0.3))
+                bone_1 = Bone(sc_1, bone_parent, position=(0,i,0), scale=(0.3, 1, 0.3))
+                bone_parent.children.append(bone_1)
             self.bones.append(bone_1)
- 
+
+        for i in range(2):
+            sc_1 = SceneObject(mesh, light, position=(0,0,0), scale=(0.3, 1, 0.3))
+            self.scene_objects.append(sc_1)
+            bone_parent = self.bones[1]
+            bone_1 = Bone(sc_1, bone_parent, position=(0,i,0), scale=(0.3, 1, 0.3))
+            bone_parent.children.append(bone_1)
+            self.bones.append(bone_1)
+
+        for i in range(2):
+            sc_1 = SceneObject(mesh, light, position=(0,0,0), scale=(0.3, 1, 0.3))
+            self.scene_objects.append(sc_1)
+            bone_parent = self.bones[3]
+            bone_1 = Bone(sc_1, bone_parent, position=(0,i,0), scale=(0.3, 1, 0.3))
+            bone_parent.children.append(bone_1)
+            self.bones.append(bone_1)
+
+
     def update(self, time:int) -> None:
+        self.bones[0].set_position(np.array([0, 2, 0], dtype=np.float32))
+
+        #head
+        self.bones[0].set_rotation(np.array([0, 0, 1.5], dtype=np.float32))
+        self.bones[1].set_rotation(np.array([0, 0, 1.6], dtype=np.float32))
+
+        #spine
+        self.bones[2].set_rotation(np.array([ 0, 0, -1.5], dtype=np.float32))
+        self.bones[3].set_rotation(np.array([ 0, 0, 0], dtype=np.float32))
+
+        #legs
+        self.bones[-4].set_rotation(np.array([ 0.6, 0, 0], dtype=np.float32))
+        self.bones[-3].set_rotation(np.array([-0.6, 0, 0], dtype=np.float32))
+
+        self.bones[-2].set_rotation(np.array([ 0.6, 0, 1.5], dtype=np.float32))
+        self.bones[-1].set_rotation(np.array([-0.6, 0, 1.5], dtype=np.float32))
+
+        rotation_angle = 0.4*math.sin(time*8)
+        new_rotation =  np.array([0, rotation_angle, 0], dtype=np.float32)
+        self.bones[4].set_rotation(new_rotation)
         
-        # self.bones[0].start[2] = math.cos(time*2)
-        for id, bone in enumerate(self.bones[:]):
-            bone:Bone
-            bone.scene_object.position = bone.get_position()
-            
-            if id == 0:
-                bone.rotation = np.array([0.1*math.sin(time*2), 0, 0], dtype=np.float32)
-            else :
-                bone.rotation = np.array([0, 0, 0], dtype=np.float32)
-            bone.create_matrix()
-            
-            rotation = bone.rotation.copy()
-            parent = bone.parent
-            while parent is not None:
-                rotation += parent.rotation
-                parent = parent.parent
-            # rotation = np.array([rotation, 0, 0], dtype=np.float32)
+        for id, bone in enumerate(self.bones[-4:]):
+            rotation_angle = 0.2*math.sin(time*3)
+            new_rotation = bone.rotation + np.array([0, 0, rotation_angle], dtype=np.float32)
+            bone.set_rotation(new_rotation)
 
-            bone.scene_object.rotation = rotation
-
-
-            # bone.start[1] += math.sin(time*0.01)
-            # bone.scene_object.position[1] += 0.01*math.sin(time*10)
-        pass
 
